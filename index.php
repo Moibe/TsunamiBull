c04580980247719517d489041eb321f177fbc36c
Moibe <moi_estrello@hotmail.com>
2021-12-03 14:22:41 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="UTF-8" />
+      <title>Tsunami Bull Crypto Index</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="25cde44.css" />
+      <link rel="icon" type="image/x-icon" href="/tsunamibull.png" />
+      
+      
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-54H457Z');</script>
+<!-- End Google Tag Manager -->
+   </head>
+   <body>
+   <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-54H457Z"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+      <div id="page" class="site">
+      <div id="content" class="site-content">
+         <div class="row">
+            <div id="primary" class="col-xs-12 col-md-12">
+               <div id="before-loop-area" class="before-loop-area widget-area"></div>
+               <main id="main" class="site-main" role="main">
+                  <article id="post-4059" class="post-4059 page type-page status-publish hentry no-thumb">
+                     <div class="entry-content">
+                        <div class="elementor elementor-4059">
+                           <div class="elementor-inner">
+                              <div class="elementor-section-wrap">
+                                 <section data-id="jxhfrug" class="full-screen elementor-element elementor-element-jxhfrug elementor-section-boxed elementor-section-height-default elementor-section-height-default invert elementor-section elementor-top-section" data-settings="{&quot;background_background&quot;:&quot;classic&quot;}" data-element_type="section">
+                                    <div class="elementor-background-overlay"></div>
+                                    <div class="elementor-container elementor-column-gap-default">
+                                       <div class="elementor-row">
+                                          <div data-id="bpe232v" class="elementor-element elementor-element-bpe232v elementor-column elementor-col-100 elementor-top-column" data-element_type="column">
+                                             <div class="elementor-column-wrap elementor-element-populated">
+                                                <div class="elementor-widget-wrap">
+                                                   <div data-id="j0hc5x6" class="elementor-element elementor-element-j0hc5x6 elementor-widget elementor-widget-heading" data-element_type="heading.default">
+                                                    <br>
+                                                    <h4 style="  color: yellow;  font-size: 20px;">
+                                                    <img src="/whale.png" /><b> Tsunami Bull Index ® </b><img src="/tsunamibull.png" /><br>
+                                                    </h4> 
+                                                  
+                                                      
+                                                   <div data-id="91przpf" class="elementor-element elementor-element-91przpf elementor-widget elementor-widget-divider" data-element_type="divider.default">
+                                                      
+                                                      <h4 style="  color: #33CAFF;  font-size: 20px;">
+                                                      <b>Best 3 Crypto Winners in the Last 24 hours</b><br>
+                                                      </h4>
+                                                      <h4 style="  color: #D169B5;  font-size: 17px;">
+                                                      <b>Simple - Robust - Real Time</b>
+                                                      </h4>
+                                                                                                         <p style="font-size: 14px;"><b>Last Update : <span style="font-size: 14px;" id="datetime"></span></b></p>
+                                                            <span class="elementor-divider-separator"></span>
+                                                            <br>
+                                                         
+                                                      <section data-id="44gcpep" class="elementor-element elementor-element-44gcpep elementor-section-boxed elementor-section-height-default elementor-section-height-default elementor-section elementor-inner-section" data-element_type="section">
+                                                         <div class="elementor-container elementor-column-gap-default">
+                                                            <div class="elementor-row">
+                                                               <?php $token=file_get_contents("https://masternode.id/resultcoins");
+                                                               $token=json_decode($token);
+                                                               foreach($token as $key){ ?>
+                                                               <div data-id="c1p8342" class="elementor-element elementor-element-c1p8342 elementor-column elementor-col-33 elementor-inner-column" data-element_type="column">
+                                                                  <div class="elementor-column-wrap elementor-element-populated">
+                                                                     <div class="elementor-widget-wrap">
+                                                                        <div data-id="6hearvn" class="elementor-element elementor-element-6hearvn elementor-view-framed elementor-shape-circle elementor-position-top elementor-vertical-align-top elementor-widget elementor-widget-icon-box" data-element_type="icon-box.default">
+                                                                           <div class="elementor-widget-container">
+                                                                              <div class="elementor-icon-box-wrapper">
+                                                                                 <div class="elementor-icon-box-content">
+                                                                                    <h4 class="elementor-icon-box-title">
+                                                                                       <span><b><?php echo $key->name;?></b></span>
+                                                                                    </h4>
+
+                                                                                 <div class="elementor-icon-box-icon">
+                                                                                    <span class="elementor-icon elementor-animation-">
+                                                                                    <img src="<?php echo $key->logo;?>" style='width:64px' />
+                                                                                    </span>
+                                                                                 </div>
+                                                                                    <b><span STYLE="font-size:17px">
+                                                                                       Symbol:<?php echo $key->coin;?></span></b>
+                                                                                       <br><br>
+                                                                                       <span style="    font-size: 16px;">Last 24h change:</span>
+                                                                                    <h4 style="  color: yellow;  font-size: 20px;">
+                                                                                     <b>  + <?php echo number_format($key->change24,2);?> % </b>
+                                                                                       <!-- <img src="arrow.jpg" height="25px" width="25px"> -->
+                                                                                    </h4>
+                                                                                    <h4 class="elementor-icon-box-title" style="    font-size: 16px;">
+                                                                                       <b>Price: $<span><?php echo number_format($key->price,2);?> USD</span></b>
+                                                                                    </h4>
+                                                                                    
+                                                                                    <div style="  color: #6ECA7F;  font-size: 17px; >
+                                                                                       <p class="elementor-icon-box-description">
+                                                                                          <b>Total Market Capitalization</b><br> $ <?php echo number_format($key->market_cap,2);?> USD
+                                                                                       </p>
+                                                                                       
+                                                                                    </div>
+
+                                                                                   
+
+                                                                                 </div>
+                                                                                 
+                                                                              </div>
+                                                                           </div>
+                                                                        </div>
+                                                                     </div>
+                                                                  </div>
+                                                               </div>
+                                                               <?php } ?>
+                                                            </div>
+                                                         </div>
+                                                      </section>
+                                                   </div>
+                                                   <p style="font-size: 16px; text-align:center;">
+                                                      <b>Tsunami Bull is a robust index that monitors top crypto in real time. It includes cryptocurrencies, derivatives, storage, DeFi, 
+                                                      Mining and Yield Farming. 
+                                                   <br>   
+                                                   The index shows the top 3 cryptos in the last 24 hours based in this monitored factors: a) Price hike 
+                                                      b) Transaction volume c) Market capitalization d) Mining or staking options e) Listings in the top exchanges f) Speed of transactions 
+                                                      g) Coin maturity h) Coin or asset i) Premined status and j) Worldwide financial and governance status.  
+                                                      <br>
+                                                      Tsunami Bull Index updates at each new BTC block (10 minutes approx). Remember to bookmark this site Ctrl + D.</b>
+                                                      </p> 
+                                                     
+                                                         <p>
+                                                         The information exposed in this website is specific historical data for technical informational purposes only and are not intended to provide specific advice of future projections for any individual or on any specific security or investment product.
+                                                         </p>
+                                                </div>
+                                                
+                                             </div>
+                                          </div>
+                                       </div>
+                                 </section>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <!-- .entry-content -->
+                  </article>
+                  <!-- #post-## -->
+               </main>
+               <!-- #main -->
+               </div><!-- #primary -->
+            </div>
+            <!-- .row -->
+         </div>
+         <!-- #content -->
+      </div>
+      <!-- #page -->
+      <script src="9daec9b.js"></script>
+      <script>
+var dt = new Date();
+var min = dt.getUTCMinutes(min);
+var ran = Math.floor((Math.random() * min) + 1)
+dt.setUTCMinutes(ran);
+var options = {dateStyle: 'medium',  timeStyle:	'full'}
+document.getElementById("datetime").innerHTML = dt.toLocaleString('en-GB', options);
+</script>
+</body>
+</html>
